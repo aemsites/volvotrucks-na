2410976b35f2c027ff720320eea44e582e788dc2
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2025-03-27 15:50:35 +0100

    add JSDocs to all functions in the custom dropdown #230

--- common/custom-dropdown/custom-dropdown.js
+++ common/custom-dropdown/custom-dropdown.js
@@ -22,16 +22,40 @@ const SelectActions = {
   Type: 10,
 };
 
+/**
+ * Get the value of an option.
+ *
+ * @param {string | Object} option - The option to get the value from.
+ * @returns {string} - The value of the option.
+ */
 function getOptionValue(option) {
   return typeof option === 'string' ? option : option.value;
 }
 
+/**
+ * Get the label of an option.
+ *
+ * @param {string | Object} option - The option to get the label from.
+ * @returns {string} - The label of the option.
+ */
 function getOptionLabel(option) {
   return typeof option === 'string' ? option : option.label;
 }
 
-// filter an array of options against an input string
-// returns an array of options that begin with the filter string, case-independent
+/**
+ * Filter an array of options against an input string.
+ *
+ * @param {string[]} options - The list of options to filter.
+ * @param {string} filter - The search string to filter the options.
+ * @param {string[]} [exclude=[]] - The list of options to exclude from the filtered list.
+ * @returns {string[]} - Returns an array of options that begin with the filter string, case-independent.
+ * @example
+ * const options = ['Apple', 'Banana', 'Cherry'];
+ * const filter = 'a';
+ * const exclude = ['Banana'];
+ * const filteredOptions = filterOptions(options, filter, exclude);
+ * // expected output: ['Apple']
+ */
 function filterOptions(options = [], filter, exclude = []) {
   return options.filter((option) => {
     const matches = option.toLowerCase().indexOf(filter.toLowerCase()) === 0;
@@ -39,10 +63,17 @@ function filterOptions(options = [], filter, exclude = []) {
   });
 }
 
-// map a key press to an action
+/**
+ * Map the key pressed to perform an action.
+ *
+ * @param {KeyboardEvent} event - The keyboard event.
+ * @param {boolean} menuOpen - If the dropdown menu is open or not.
+ * @returns {SelectActions} - The action to perform based on the key pressed.
+ */
 function getActionFromKey(event, menuOpen) {
   const { key, altKey, ctrlKey, metaKey } = event;
-  const openKeys = ['ArrowDown', 'ArrowUp', 'Enter', ' ']; // all keys that will do the default open action
+  // all keys that will do the default open action
+  const openKeys = ['ArrowDown', 'ArrowUp', 'Enter', ' '];
   // handle opening when closed
   if (!menuOpen && openKeys.includes(key)) {
     return SelectActions.Open;
@@ -81,8 +112,21 @@ function getActionFromKey(event, menuOpen) {
   }
 }
 
-// return the index of an option from an array of options, based on a search string
-// if the filter is multiple iterations of the same letter (e.g "aaa"), then cycle through first-letter matches
+/**
+ * Get the index of an option from an array of options, based on a search string.
+ * if the filter has multiple iterations of the same letter (e.g "aaa"), then cycle through first-letter matches
+ *
+ * @param {string[]} options - The list of options to search.
+ * @param {string} filter - The search string to filter the options.
+ * @param {number} [startIndex=0] - The index to start searching from.
+ * @returns {number} - The index of the option from the list of options.
+ * @example
+ * const options = ['Apple', 'Banana', 'Cherry'];
+ * const filter = 'a';
+ * const startIndex = 0;
+ * const index = getIndexByLetter(options, filter, startIndex);
+ * // expected output: 0
+ */
 function getIndexByLetter(options, filter, startIndex = 0) {
   const orderedOptions = [...options.slice(startIndex), ...options.slice(0, startIndex)];
   const firstMatch = filterOptions(orderedOptions, filter)[0];
@@ -105,7 +149,20 @@ function getIndexByLetter(options, filter, startIndex = 0) {
   }
 }
 
-// get an updated option index after performing an action
+/**
+ * Get the updated index of an option after performing an action.
+ *
+ * @param {number} currentIndex - The current index of the option.
+ * @param {number} maxIndex - The maximum index of the options.
+ * @param {SelectActions} action - The action to perform.
+ * @returns {number} - The updated index of the option.
+ * @example
+ * const currentIndex = 0;
+ * const maxIndex = 2;
+ * const action = SelectActions.Next;
+ * const updatedIndex = getUpdatedIndex(currentIndex, maxIndex, action);
+ * // expected output: 1
+ */
 function getUpdatedIndex(currentIndex, maxIndex, action) {
   const pageSize = 10; // used for pageup/pagedown
 
@@ -127,7 +184,12 @@ function getUpdatedIndex(currentIndex, maxIndex, action) {
   }
 }
 
-// check if element is visible in browser view port
+/**
+ * Check if an element is currently in the view port.
+ *
+ * @param {HTMLElement} element - The element to check if it is in the view port.
+ * @returns {boolean} - Returns true if the element is in the view port, otherwise false.
+ */
 function isElementInView(element) {
   const bounding = element.getBoundingClientRect();
 
@@ -139,13 +201,23 @@ function isElementInView(element) {
   );
 }
 
-// check if an element is currently scrollable
+/**
+ * check if an element is currently scrollable
+ *
+ * @param {HTMLElement} element - The element to check if it is scrollable.
+ * @returns {boolean} - Returns true if the element is scrollable, otherwise false.
+ */
 function isScrollable(element) {
   return element && element.clientHeight < element.scrollHeight;
 }
 
-// ensure a given child element is within the parent's visible scroll area
-// if the child is not visible, scroll the parent
+/**
+ * Ensure a given child element is within the parent's visible scroll area.
+ * If the child is not visible, scroll the parent.
+ *
+ * @param {HTMLElement} activeElement - The child element to ensure is visible.
+ * @param {HTMLElement} scrollParent - The parent element to scroll if the child is not visible.
+ */
 function maintainScrollVisibility(activeElement, scrollParent) {
   const { offsetHeight, offsetTop } = activeElement;
   const { offsetHeight: parentOffsetHeight, scrollTop } = scrollParent;
@@ -160,9 +232,14 @@ function maintainScrollVisibility(activeElement, scrollParent) {
   }
 }
 
-/*
+/**
  * Select Component
  * Accepts a clickable element and an array of string options
+ *
+ * @param {HTMLElement} el - The element to attach the select component to.
+ * @param {string[]} options - The list of options to display in the select.
+ * @param {string} placeholder - The placeholder text for the select.
+ * @param {function} onChangeCallback - The callback function to be called when the value changes.
  */
 const Select = function (el, options = [], placeholder, onChangeCallback) {
   // element refs
@@ -189,6 +266,9 @@ const Select = function (el, options = [], placeholder, onChangeCallback) {
   }
 };
 
+/**
+ * Initialize the select component.
+ */
 Select.prototype.init = function init() {
   // select first option by default
   this.buttonEl.innerHTML = getOptionLabel(this.placeholder || this.options[0]);
@@ -206,6 +286,13 @@ Select.prototype.init = function init() {
   });
 };
 
+/**
+ * Create an option element.
+ *
+ * @param {string} option - The option to create an element for.
+ * @param {number} index - The index of the option in the list.
+ * @returns {HTMLElement} - The option element.
+ */
 Select.prototype.createOption = function createOption(option, index) {
   const optionEl = createElement('div');
   optionEl.setAttribute('role', 'option');
@@ -229,6 +316,12 @@ Select.prototype.createOption = function createOption(option, index) {
   return optionEl;
 };
 
+/**
+ * Get the search string by adding the most recent letter to the saved search string.
+ *
+ * @param {string} char - The most recent letter to add to the search string.
+ * @returns {string} - The search string.
+ */
 Select.prototype.getSearchString = function getSearchString(char) {
   // reset typing timeout and start new timeout
   // this allows us to make multiple-letter matches, like a native select
@@ -245,6 +338,11 @@ Select.prototype.getSearchString = function getSearchString(char) {
   return this.searchString;
 };
 
+/**
+ * Handle the blur event on the button.
+ *
+ * @param {FocusEvent} event - The blur event.
+ */
 Select.prototype.onButtonBlur = function onButtonBlur(event) {
   // do nothing if relatedTarget is contained within listEl
   if (this.listEl.contains(event.relatedTarget)) {
@@ -257,10 +355,18 @@ Select.prototype.onButtonBlur = function onButtonBlur(event) {
   }
 };
 
+/**
+ * Handle the click event on the button.
+ */
 Select.prototype.onButtonClick = function onButtonClick() {
   this.updateMenuState(!this.open, false);
 };
 
+/**
+ * Handle the keydown event on the button.
+ *
+ * @param {KeyboardEvent} event - The keydown event.
+ */
 Select.prototype.onButtonKeyDown = function onButtonKeyDown(event) {
   const { key } = event;
   const max = this.options.length - 1;
@@ -293,6 +399,11 @@ Select.prototype.onButtonKeyDown = function onButtonKeyDown(event) {
   }
 };
 
+/**
+ * Handle the type event on the button.
+ *
+ * @param {string} letter - The letter to type.
+ */
 Select.prototype.onButtonType = function onButtonType(letter) {
   // open the listbox if it is closed
   this.updateMenuState(true);
@@ -312,6 +423,11 @@ Select.prototype.onButtonType = function onButtonType(letter) {
   }
 };
 
+/**
+ * Handle the change event on an option.
+ *
+ * @param {number} index - The index of the option that changed.
+ */
 Select.prototype.onOptionChange = function onOptionChange(index) {
   // update state
   this.activeIndex = index;
@@ -338,18 +454,32 @@ Select.prototype.onOptionChange = function onOptionChange(index) {
   }
 };
 
+/**
+ * Handle the click event on an option.
+ *
+ * @param {number} index - The index of the option that was clicked.
+ * @param {MouseEvent} event - The click event.
+ */
 Select.prototype.onOptionClick = function onOptionClick(index) {
   this.onOptionChange(index);
   this.selectOption(index);
   this.updateMenuState(false);
 };
 
+/**
+ * Handle the mousedown event on an option.
+ */
 Select.prototype.onOptionMouseDown = function onOptionMouseDown() {
   // Clicking an option will cause a blur event,
   // but we don't want to perform the default keyboard blur action
   this.ignoreBlur = true;
 };
 
+/**
+ * create an option item from the select element.
+ *
+ * @param {number} index - The index of the option to select.
+ */
 Select.prototype.selectOption = function selectOption(index) {
   // update state
   this.activeIndex = index;
@@ -375,6 +505,12 @@ Select.prototype.selectOption = function selectOption(index) {
   }
 };
 
+/**
+ * Update the state of the dropdown menu.
+ *
+ * @param {boolean} open - If the dropdown menu is open or not.
+ * @param {boolean} [callFocus=true] - If the focus should be called on the button.
+ */
 Select.prototype.updateMenuState = function updateMenuState(open, callFocus = true) {
   if (this.open === open) {
     return;
@@ -415,10 +551,24 @@ const createOptionMarkup = (idx, option, hasPlaceholder) => {
       </option>`;
 };
 
+/**
+ * Generates the markup for a list of HTML option elements.
+ *
+ * @param {string[]|Object[]} list - The list of options to generate the markup for.
+ * @param {boolean} hasPlaceholder - If the list has a placeholder option.
+ * @returns {string} The HTML markup for the list of option elements.
+ */
 const createSelectHtml = (list, hasPlaceholder) => {
   return list.map((item, idx) => createOptionMarkup(idx, item, hasPlaceholder)).join('');
 };
 
+/**
+ * Get the variant classes for the dropdown.
+ *
+ * @param {Object} options - The options to configure the dropdown.
+ * @param {string[]} options.variants - The list of variants to apply to the dropdown.
+ * @returns {string} The variant classes for the dropdown.
+ */
 function getVariantClasses(options) {
   const { variants = [] } = options;
   if (!Array.isArray(variants)) {
