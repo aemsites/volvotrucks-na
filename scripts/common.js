483923927e923389b30164b9da81211a51b27976
santi-homps <santiagohomps@gmail.com>
2025-07-03 15:02:55 -0300

    testing

--- scripts/common.js
+++ scripts/common.js
@@ -647,16 +647,23 @@ export function createResponsivePicture(images, eager, alt, imageClass) {
 const { GTM_ID = false } = COOKIE_CONFIGS;
 
 // Google Analytics
-// async function loadGoogleTagManager() {
-// google tag manager
-(function loadGoogleTagManagerInit(w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  const f = d.getElementsByTagName(s)[0];
-  const j = d.createElement(s);
-  const dl = l !== 'dataLayer' ? `&l=${l}` : '';
-  j.async = true;
-  j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
-  f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', GTM_ID);
-// }
+function loadGoogleTagManager() {
+  console.log(GTM_ID)(
+    // google tag manager
+    function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js',
+      });
+      const f = d.getElementsByTagName(s)[0];
+      const j = d.createElement(s);
+      const dl = l != 'dataLayer' ? `&l=${l}` : '';
+      j.async = true;
+      j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
+      f.parentNode.insertBefore(j, f);
+    },
+  )(window, document, 'script', 'dataLayer', GTM_ID);
+}
+
+loadGoogleTagManager();
