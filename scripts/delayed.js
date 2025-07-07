9045afe307920488b0b43656435fc71006769a12
santi-homps <santiagohomps@gmail.com>
2025-07-07 15:03:08 -0300

    test

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -101,9 +101,9 @@ document.addEventListener('click', (e) => {
 })();
 
 let referrer;
-
+const test = 2;
 // OneTrust Cookies Consent Notice start for volvotrucks.us
-if (DATA_DOMAIN_SCRIPT && !window.location.pathname.includes('srcdoc')) {
+if (test === 2 || (DATA_DOMAIN_SCRIPT && !window.location.pathname.includes('srcdoc'))) {
   // when running on localhost in the block library host is empty but the path is srcdoc
   // on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens,
   // because the cookie is not persistent. To avoid this annoyance, disable unless on the
@@ -130,8 +130,8 @@ if (DATA_DOMAIN_SCRIPT && !window.location.pathname.includes('srcdoc')) {
         return;
       }
       if (!isSameGroups(currentOnetrustActiveGroups, window.OnetrustActiveGroups) && window.isSingleVideo !== 'true') {
-        console.warn(document.referrer);
         referrer = document.referrer;
+        console.warn(referrer);
         window.location.reload();
       }
     });
