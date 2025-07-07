5d97bbae3edd561ff6e4e78f69e28ce5f63538bb
santi-homps <santiagohomps@gmail.com>
2025-07-07 12:45:06 -0300

    remove devhost

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -101,7 +101,7 @@ document.addEventListener('click', (e) => {
 })();
 
 // OneTrust Cookies Consent Notice start for volvotrucks.us
-if (DATA_DOMAIN_SCRIPT && !window.location.pathname.includes('srcdoc') && !isDevHost()) {
+if (DATA_DOMAIN_SCRIPT && !window.location.pathname.includes('srcdoc')) {
   // when running on localhost in the block library host is empty but the path is srcdoc
   // on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens,
   // because the cookie is not persistent. To avoid this annoyance, disable unless on the
