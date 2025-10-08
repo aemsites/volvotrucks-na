f8815f36a01927cdb3fa9408dd541fb5e1cb372b
Alexis Coelho <alexis.coelho@gmail.com>
2025-10-08 09:36:31 +0200

    Minor improvements #513

--- tools/sidekick/aem-genai-variations.js
+++ tools/sidekick/aem-genai-variations.js
@@ -1,14 +1,14 @@
-(function () {
+(function AemGenAiVariationsloadScript() {
   let isAEMGenAIVariationsAppLoaded = false;
 
   function loadAEMGenAIVariationsApp() {
     const script = document.createElement('script');
     script.src =
       'https://experience.adobe.com/solutions/aem-sites-genai-aem-genai-variations-mfe/static-assets/resources/sidekick/client.js?source=plugin';
-    script.onload = function () {
+    script.onload = function loadAEMGenAIVariationsAppOnLoad() {
       isAEMGenAIVariationsAppLoaded = true;
     };
-    script.onerror = function () {
+    script.onerror = function loadAEMGenAIVariationsAppOnError() {
       console.error('Error loading AEMGenAIVariationsApp.');
     };
     document.head.appendChild(script);
@@ -21,7 +21,7 @@
   }
 
   // The code snippet for the Sidekick V1 extension, https://chromewebstore.google.com/detail/aem-sidekick/ccfggkjabjahcjoljmgmklhpaccedipo?hl=en
-  const sidekick = document.querySelector('helix-sidekick');
+  let sidekick = document.querySelector('helix-sidekick');
   if (sidekick) {
     // sidekick already loaded
     sidekick.addEventListener('custom:aem-genai-variations-sidekick', handlePluginButtonClick);
@@ -30,14 +30,17 @@
     document.addEventListener(
       'sidekick-ready',
       () => {
-        document.querySelector('helix-sidekick').addEventListener('custom:aem-genai-variations-sidekick', handlePluginButtonClick);
+        sidekick = document.querySelector('helix-sidekick');
+        if (sidekick) {
+          sidekick.addEventListener('custom:aem-genai-variations-sidekick', handlePluginButtonClick);
+        }
       },
       { once: true },
     );
   }
 
   // The code snippet for the Sidekick V2 extension, https://chromewebstore.google.com/detail/aem-sidekick/igkmdomcgoebiipaifhmpfjhbjccggml?hl=en
-  const sidekickV2 = document.querySelector('aem-sidekick');
+  let sidekickV2 = document.querySelector('aem-sidekick');
   if (sidekickV2) {
     // sidekick already loaded
     sidekickV2.addEventListener('custom:aem-genai-variations-sidekick', handlePluginButtonClick);
@@ -46,7 +49,10 @@
     document.addEventListener(
       'sidekick-ready',
       () => {
-        document.querySelector('aem-sidekick').addEventListener('custom:aem-genai-variations-sidekick', handlePluginButtonClick);
+        sidekickV2 = document.querySelector('aem-sidekick');
+        if (sidekickV2) {
+          sidekickV2.addEventListener('custom:aem-genai-variations-sidekick', handlePluginButtonClick);
+        }
       },
       { once: true },
     );
