68496717f56523b8f479f0b8b33185f1c7a4aacd
amglebihan <166131459+amglebihan@users.noreply.github.com>
2025-11-13 11:17:34 +0100

    568 - Restore tabbed accordion and carousel

---
+++ blocks/tabbed-accordion/tabbed-carousel.js
@@ -0,0 +1,56 @@
+function buildTabNavigation(tabItems, clickHandler) {
+  const tabNavigation = document.createElement('ul');
+  [...tabItems].forEach((tabItem, i) => {
+    const listItem = document.createElement('li');
+    if (!i) {
+      listItem.classList.add('active');
+    }
+    const button = document.createElement('button');
+    const tabContent = tabItem.querySelector(':scope > div, :scope > p > div');
+    if (tabContent) {
+      button.addEventListener('click', () => clickHandler.call(this, tabContent, tabNavigation, listItem));
+      button.innerHTML = tabContent.dataset.accordion;
+    }
+    listItem.append(button);
+    tabNavigation.append(listItem);
+  });
+  tabNavigation.className = 'tab-navigation';
+  return tabNavigation;
+}
+
+export default function decorate(block) {
+  const tabContainer = block.querySelector(':scope > div, :scope > p > div');
+  tabContainer.classList.add('accordion-container');
+  const tabItems = block.querySelectorAll(':scope > div > div');
+  const tabNavigation = buildTabNavigation(tabItems, (tabContent, nav, listItem) => {
+    const currentActiveTabContent = tabContainer.querySelector('.is-active');
+    if (currentActiveTabContent !== tabContent) {
+      currentActiveTabContent.classList.remove('is-active');
+    }
+    tabContent.parentElement.classList.add('is-active');
+
+    const currentActiveNavigationItem = nav.querySelector('.active');
+    if (currentActiveNavigationItem !== listItem) {
+      currentActiveNavigationItem.classList.remove('active');
+    }
+    listItem.classList.add('active');
+  });
+  tabItems.forEach((tabItem, i) => {
+    tabItem.classList.add('accordion-item');
+    const accordionContent = tabItem.querySelector(':scope > div, :scope > p > div');
+    accordionContent.classList.add('accordion-collapse');
+    const accordionHeader = document.createElement('div');
+    accordionHeader.classList.add('accordion-header');
+    if (!i) {
+      tabItem.classList.add('is-active');
+    }
+    const button = document.createElement('button');
+    button.innerHTML = accordionContent.dataset.accordion;
+    accordionHeader.append(button);
+    button.addEventListener('click', () => {
+      tabItem.classList.toggle('is-active');
+    });
+    tabItem.prepend(accordionHeader);
+  });
+  block.parentElement.prepend(tabNavigation);
+}
---
+++ blocks/tabbed-carousel/tabbed-carousel.js
@@ -0,0 +1,101 @@
+const debounceDelay = 30;
+
+function stripEmptyTags(main, child) {
+  if (child !== main && child.innerHTML.trim() === '') {
+    const parent = child.parentNode;
+    child.remove();
+    stripEmptyTags(main, parent);
+  }
+}
+
+function setActive(tabNavigation, tabContainer, index) {
+  if (!tabNavigation.children[index].classList.contains('active')) {
+    [tabNavigation, tabContainer].forEach((c) => c.querySelectorAll('.active').forEach((i) => i.classList.remove('active')));
+
+    handlingVideo(tabContainer, index);
+    tabNavigation.children[index].classList.add('active');
+    tabContainer.children[index].classList.add('active');
+  }
+}
+
+function buildTabNavigation(tabItems, clickHandler) {
+  const tabNavigation = document.createElement('ul');
+  [...tabItems].forEach((tabItem, i) => {
+    const listItem = document.createElement('li');
+    const button = document.createElement('button');
+    button.addEventListener('click', () => clickHandler.call(this, tabNavigation, tabItem, i));
+    const tabContent = tabItem.querySelector(':scope > div');
+    button.innerHTML = tabContent.dataset.carousel;
+    listItem.append(button);
+    tabNavigation.append(listItem);
+  });
+  tabNavigation.className = 'tab-navigation';
+  return tabNavigation;
+}
+
+function handlingVideo(carouselContainer, activeIndex) {
+  carouselContainer.querySelectorAll('.tab-item').forEach((tab, index) => {
+    const video = tab.querySelector('video');
+
+    if (video) {
+      if (index === activeIndex) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    }
+  });
+}
+
+export default function decorate(block) {
+  const tabContainer = block.querySelector(':scope > div');
+  tabContainer.classList.add('tab-container');
+  const tabItems = block.querySelectorAll(':scope > div > div');
+
+  tabItems.forEach((tabItem) => {
+    const firstChildParagraph = tabItem.querySelector(':scope > p');
+    if (firstChildParagraph) {
+      tabItem.innerHTML = firstChildParagraph.innerHTML;
+    }
+  });
+
+  const tabNavigation = buildTabNavigation(tabItems, (nav, tabItem, index) => {
+    tabContainer.scrollTo({
+      top: 0,
+      left: tabItem.offsetLeft - tabItem.parentNode.offsetLeft,
+      behavior: 'smooth',
+    });
+
+    setActive(tabNavigation, tabContainer, index);
+  });
+  tabItems.forEach((tabItem) => {
+    tabItem.classList.add('tab-item');
+    const tabContent = tabItem.querySelector(':scope > div');
+    const picture = tabItem.querySelector('picture');
+
+    if (picture) {
+      tabContent.prepend(picture);
+    }
+
+    tabContent.querySelectorAll('p, div').forEach((item) => {
+      stripEmptyTags(tabContent, item);
+    });
+  });
+  block.parentElement.prepend(tabNavigation);
+
+  // update the button indicator on scroll
+  let scrollTimeout;
+  tabContainer.addEventListener('scroll', () => {
+    if (scrollTimeout) {
+      clearTimeout(scrollTimeout);
+    }
+    scrollTimeout = setTimeout(() => {
+      const { scrollLeft } = tabContainer;
+      const { clientWidth } = tabContainer.firstElementChild;
+      const activeIndex = Math.floor(scrollLeft / clientWidth);
+      setActive(tabNavigation, tabContainer, activeIndex);
+    }, debounceDelay);
+  });
+
+  setActive(tabNavigation, tabContainer, 0);
+}
